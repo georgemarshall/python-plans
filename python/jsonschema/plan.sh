a016cfe5503325f84f666c53fa0f98083b810565
George Marshall <george@georgemarshall.name>
2017-01-22 22:28:53 -0800

    Added plan for jsonschema
    Signed-off-by: George Marshall <george@georgemarshall.name>

---
+++ python/jsonschema/plan.sh
@@ -0,0 +1,11 @@
+source ../../packages/jsonschema.sh
+source ../default.sh
+
+pkg_deps=(
+  ${pkg_base_deps[@]}
+  python/python
+)
+pkg_build_deps=(
+  ${pkg_base_build_deps[@]}
+  python/setuptools
+)
