31ebccb4cb6f1ff062cd410db6a00677b48024f8
George Marshall <george@georgemarshall.name>
2017-01-22 22:31:21 -0800

    Added do_check() to python/python2 wheel plans
    Signed-off-by: George Marshall <george@georgemarshall.name>

--- python/wheel/plan.sh
+++ python/wheel/plan.sh
@@ -2,6 +2,19 @@ source ../../packages/wheel.sh
 source ../default.sh
 
 pkg_deps=(
+  ${pkg_base_deps[@]}
   python/python
   python/setuptools
 )
+pkg_build_deps=(
+  ${pkg_base_build_deps[@]}
+  python/coverage
+  python/jsonschema
+  python/py
+  python/pytest
+  python/pytest-cov
+)
+
+do_check() {
+  python setup.py test
+}
--- python2/wheel/plan.sh
+++ python2/wheel/plan.sh
@@ -2,6 +2,19 @@ source ../../packages/wheel.sh
 source ../default.sh
 
 pkg_deps=(
+  ${pkg_base_deps[@]}
   python2/python
   python2/setuptools
 )
+pkg_build_deps=(
+  ${pkg_base_build_deps[@]}
+  python2/coverage
+  python2/jsonschema
+  python2/py
+  python2/pytest
+  python2/pytest-cov
+)
+
+do_check() {
+  python setup.py test
+}
