2552a1985cdc1a7057f68e3fd6e6777b7ca42fd1
George Marshall <george@georgemarshall.name>
2017-01-22 20:20:48 -0800

    Changed pillow plans to use python/nose and python2/nose
    Signed-off-by: George Marshall <george@georgemarshall.name>

--- python/pillow/plan.sh
+++ python/pillow/plan.sh
@@ -7,17 +7,10 @@ pkg_deps=(
 )
 pkg_build_deps=(
   ${pkg_base_build_deps[@]}
-  python/pip
+  python/nose
   python/setuptools
 )
 
 do_check() {
-  pip install nose
   python setup.py test
-}
-
-do_end(){
-  if [[ -n "$DO_CHECK" ]]; then
-    pip uninstall --yes nose
-  fi
 }
--- python2/pillow/plan.sh
+++ python2/pillow/plan.sh
@@ -7,17 +7,10 @@ pkg_deps=(
 )
 pkg_build_deps=(
   ${pkg_base_build_deps[@]}
-  python2/pip
+  python2/nose
   python2/setuptools
 )
 
 do_check() {
-  pip install nose
   python setup.py test
-}
-
-do_end(){
-  if [[ -n "$DO_CHECK" ]]; then
-    pip uninstall --yes nose
-  fi
 }
