3b82bfe9d8f64fe2128b5cb45cc0fed2835a22e3
George Marshall <george@georgemarshall.name>
2017-04-06 00:28:11 -0700

    Update simplejson
    Signed-off-by: George Marshall <george@georgemarshall.name>

--- python2/simplejson/plan.sh
+++ python2/simplejson/plan.sh
@@ -1,15 +1,51 @@
-source ../../packages/simplejson.sh
-source ../default.sh
-
+pkg_name=simplejson
+pkg_distname=${pkg_name}
+pkg_version=3.10.0
+pkg_origin=python2
+pkg_license=('MIT' 'AFL-2.1')
+pkg_maintainer="George Marshall <george@georgemarshall.name>"
+pkg_description="Simple, fast, extensible JSON encoder/decoder for Python"
+pkg_upstream_url=http://github.com/simplejson/simplejson
+pkg_dirname=${pkg_distname}-${pkg_version}
+pkg_source=https://pypi.org/packages/source/s/simplejson/${pkg_dirname}.tar.gz
+pkg_shasum=953be622e88323c6f43fad61ffd05bebe73b9fd9863a46d68b052d2aa7d71ce2
 pkg_deps=(
-  ${pkg_base_deps[@]}
   python2/python
 )
 pkg_build_deps=(
-  ${pkg_base_build_deps[@]}
+  core/gcc
   python2/setuptools
 )
+pkg_env_sep=(
+  ['PYTHONPATH']=':'
+)
+
+do_begin() {
+  add_path_env 'PYTHONPATH' 'lib/python2.7/site-packages'
+}
+
+do_build() {
+  python setup.py build
+}
 
 do_check() {
   python setup.py test
+}
+
+do_install() {
+  python setup.py install \
+    --prefix="$pkg_prefix" \
+    --old-and-unmanageable # bypass egg install
+}
+
+do_strip() {
+  do_default_strip
+
+  # Remove tests and bytecode files
+  find "$pkg_prefix" -depth \
+    \( \
+      \( -type d -a -name test -o -name tests \) \
+      -o \
+      \( -type f -a -name '*.pyc' -o -name '*.pyo' \) \
+    \) -exec rm -rf '{}' +
 }
