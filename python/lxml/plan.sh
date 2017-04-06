69c53c13da3234c639592239dd51165bcb3700a9
George Marshall <george@georgemarshall.name>
2017-04-05 23:33:37 -0700

    Update lxml
    Signed-off-by: George Marshall <george@georgemarshall.name>

--- python/lxml/plan.sh
+++ python/lxml/plan.sh
@@ -1,16 +1,55 @@
-source ../../packages/lxml.sh
-source ../default.sh
-
+pkg_name=lxml
+pkg_distname=${pkg_name}
+pkg_version=3.7.3
+pkg_origin=python
+pkg_license=('BSD-3-Clause')
+pkg_maintainer="George Marshall <george@georgemarshall.name>"
+pkg_description="Pythonic XML processing library combining libxml2/libxslt \
+with the ElementTree API."
+pkg_upstream_url=http://lxml.de/
+pkg_dirname=${pkg_distname}-${pkg_version}
+pkg_source=https://pypi.org/packages/source/l/lxml/${pkg_dirname}.tar.gz
+pkg_shasum=aa502d78a51ee7d127b4824ff96500f0181d3c7826e6ee7b800d068be79361c7
 pkg_deps=(
-  ${pkg_base_deps[@]}
+  core/gcc-libs
+  core/libxml2
+  core/libxslt
   python/python
 )
 pkg_build_deps=(
-  ${pkg_base_build_deps[@]}
   python/cython
   python/setuptools
 )
+pkg_env_sep=(
+  ['PYTHONPATH']=':'
+)
+
+do_begin() {
+  add_path_env 'PYTHONPATH' 'lib/python3.6/site-packages'
+}
+
+do_build() {
+  python setup.py build
+}
 
 do_check() {
   python setup.py test
+}
+
+do_install() {
+  python setup.py install \
+    --prefix="$pkg_prefix" \
+    --old-and-unmanageable # bypass egg install
+}
+
+do_strip() {
+  do_default_strip
+
+  # Remove tests and bytecode files
+  find "$pkg_prefix" -depth \
+    \( \
+      \( -type d -a -name test -o -name tests \) \
+      -o \
+      \( -type f -a -name '*.pyc' -o -name '*.pyo' \) \
+    \) -exec rm -rf '{}' +
 }
