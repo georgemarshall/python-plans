639219e282c7a7ff91589d312d3a6b11b94517d9
George Marshall <george@georgemarshall.name>
2017-04-06 00:46:18 -0700

    Update jsonschema
    Signed-off-by: George Marshall <george@georgemarshall.name>

--- python2/jsonschema/plan.sh
+++ python2/jsonschema/plan.sh
@@ -1,11 +1,46 @@
-source ../../packages/jsonschema.sh
-source ../default.sh
-
+pkg_name=jsonschema
+pkg_distname=${pkg_name}
+pkg_version=2.6.0
+pkg_origin=python2
+pkg_license=('MIT')
+pkg_maintainer="George Marshall <george@georgemarshall.name>"
+pkg_description="An implementation of JSON Schema validation for Python"
+pkg_upstream_url=http://github.com/Julian/jsonschema
+pkg_dirname=${pkg_distname}-${pkg_version}
+pkg_source=https://pypi.org/packages/source/j/jsonschema/${pkg_dirname}.tar.gz
+pkg_shasum=6ff5f3180870836cae40f06fa10419f557208175f13ad7bc26caa77beb1f6e02
 pkg_deps=(
-  ${pkg_base_deps[@]}
   python2/python
 )
 pkg_build_deps=(
-  ${pkg_base_build_deps[@]}
   python2/setuptools
 )
+pkg_env_sep=(
+  ['PYTHONPATH']=':'
+)
+
+do_begin() {
+  add_path_env 'PYTHONPATH' 'lib/python2.7/site-packages'
+}
+
+do_build() {
+  python setup.py build
+}
+
+do_install() {
+  python setup.py install \
+    --prefix="$pkg_prefix" \
+    --old-and-unmanageable # bypass egg install
+}
+
+do_strip() {
+  do_default_strip
+
+  # Remove tests and bytecode files
+  find "$pkg_prefix" -depth \
+    \( \
+      \( -type d -a -name test -o -name tests \) \
+      -o \
+      \( -type f -a -name '*.pyc' -o -name '*.pyo' \) \
+    \) -exec rm -rf '{}' +
+}
