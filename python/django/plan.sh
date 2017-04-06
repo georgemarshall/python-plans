2e0c21ebb0bbf4d79d31a3a1004de11e138051c7
George Marshall <george@georgemarshall.name>
2017-04-06 01:08:46 -0700

    Add django and pytz
    Signed-off-by: George Marshall <george@georgemarshall.name>

---
+++ python/django/plan.sh
@@ -0,0 +1,47 @@
+pkg_name=django
+pkg_distname=Django
+pkg_version=1.11
+pkg_origin=python
+pkg_license=('BSD')
+pkg_maintainer="George Marshall <george@georgemarshall.name>"
+pkg_description="A high-level Python Web framework that encourages rapid \
+development and clean, pragmatic design."
+pkg_upstream_url=https://www.djangoproject.com/
+pkg_dirname=${pkg_distname}-${pkg_version}
+pkg_source=https://pypi.org/packages/source/d/django/${pkg_dirname}.tar.gz
+pkg_shasum=b6f3b864944276b4fd1d099952112696558f78b77b39188ac92b6c5e80152c30
+pkg_deps=(
+  python/python
+  python/pytz
+  python/setuptools
+)
+pkg_env_sep=(
+  ['PYTHONPATH']=':'
+)
+pkg_bin_dirs=(bin)
+
+do_begin() {
+  add_path_env 'PYTHONPATH' 'lib/python3.6/site-packages'
+}
+
+do_build() {
+  python setup.py build
+}
+
+do_install() {
+  python setup.py install \
+    --prefix="$pkg_prefix" \
+    --old-and-unmanageable # bypass egg install
+}
+
+do_strip() {
+  do_default_strip
+
+  # Remove tests and bytecode files
+  find "$pkg_prefix" -depth \
+    \( \
+      \( -type d -a -name test -o -name tests \) \
+      -o \
+      \( -type f -a -name '*.pyc' -o -name '*.pyo' \) \
+    \) -exec rm -rf '{}' +
+}
---
+++ python/pytz/plan.sh
@@ -0,0 +1,43 @@
+pkg_name=pytz
+pkg_distname=${pkg_name}
+pkg_version=2017.2
+pkg_origin=python
+pkg_license=('MIT')
+pkg_maintainer="George Marshall <george@georgemarshall.name>"
+pkg_description="World timezone definitions, modern and historical"
+pkg_upstream_url=http://pythonhosted.org/pytz
+pkg_dirname=${pkg_distname}-${pkg_version}
+pkg_source=https://pypi.org/packages/source/p/pytz/${pkg_dirname}.zip
+pkg_shasum=f5c056e8f62d45ba8215e5cb8f50dfccb198b4b9fbea8500674f3443e4689589
+pkg_deps=(
+  python/python
+)
+pkg_env_sep=(
+  ['PYTHONPATH']=':'
+)
+pkg_bin_dirs=(bin)
+
+do_begin() {
+  add_path_env 'PYTHONPATH' 'lib/python3.6/site-packages'
+}
+
+do_build() {
+  python setup.py build
+}
+
+do_install() {
+  python setup.py install \
+    --prefix="$pkg_prefix"
+}
+
+do_strip() {
+  do_default_strip
+
+  # Remove tests and bytecode files
+  find "$pkg_prefix" -depth \
+    \( \
+      \( -type d -a -name test -o -name tests \) \
+      -o \
+      \( -type f -a -name '*.pyc' -o -name '*.pyo' \) \
+    \) -exec rm -rf '{}' +
+}
