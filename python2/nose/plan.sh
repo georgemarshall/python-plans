eb2a16744642de27f2fe48fbfce3d3c76f6313ac
George Marshall <george@georgemarshall.name>
2017-04-06 00:13:37 -0700

    Update nose
    Signed-off-by: George Marshall <george@georgemarshall.name>

--- python2/nose/plan.sh
+++ python2/nose/plan.sh
@@ -1,15 +1,51 @@
-source ../../packages/nose.sh
-source ../default.sh
-
+pkg_name=nose
+pkg_distname=${pkg_name}
+pkg_version=1.3.7
+pkg_origin=python2
+pkg_license=('LGPL-2.1')
+pkg_maintainer="George Marshall <george@georgemarshall.name>"
+pkg_description="nose extends unittest to make testing easier"
+pkg_upstream_url=http://readthedocs.org/docs/nose/
+pkg_dirname=${pkg_distname}-${pkg_version}
+pkg_source=https://pypi.org/packages/source/n/nose/${pkg_dirname}.tar.gz
+pkg_shasum=f1bffef9cbc82628f6e7d7b40d7e255aefaa1adb6a1b1d26c69a8b79e6208a98
 pkg_deps=(
-  ${pkg_base_deps[@]}
   python2/python
 )
 pkg_build_deps=(
-  ${pkg_base_build_deps[@]}
   python2/setuptools
 )
+pkg_env_sep=(
+  ['PYTHONPATH']=':'
+)
+pkg_bin_dirs=(bin)
+
+do_begin() {
+  add_path_env 'PYTHONPATH' 'lib/python2.7/site-packages'
+}
+
+do_build() {
+  python setup.py build
+}
 
 do_check() {
-  python setup.py test
+  python setup.py build_tests || python setup.py egg_info; python selftest.py
+}
+
+do_install() {
+  python setup.py install \
+    --prefix="$pkg_prefix" \
+    --old-and-unmanageable # bypass egg install
+}
+
+do_strip() {
+  do_default_strip
+
+  # Remove tests and bytecode files
+  find "$pkg_prefix" -depth \
+    \( \
+      \( -type d -a -name test -o -name tests \) \
+      -o \
+      \( -type f -a -name '*.pyc' -o -name '*.pyo' \) \
+    \) -exec rm -rf '{}' +
 }
