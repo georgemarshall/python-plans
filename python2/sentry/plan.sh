500270eacc5b8bc74f724b82b05f173b63fe66b0
George Marshall <george@georgemarshall.name>
2017-04-07 01:33:24 -0700

    More setup work
    Signed-off-by: George Marshall <george@georgemarshall.name>

--- python2/sentry/plan.sh
+++ python2/sentry/plan.sh
@@ -11,9 +11,19 @@ pkg_source=https://pypi.org/packages/source/s/sentry/${pkg_dirname}.tar.gz
 pkg_shasum=f80315439197d2c70ed1e1c191731e1c2c14a80af3d7faf43a0e4ed37d5391f8
 pkg_deps=(
   python2/python
+  python2/libsourcemap
+  python2/lxml
+  python2/pillow
+  python2/psycopg2
   python2/setuptools
+  python2/simplejson
+  python2/symsynd
+  python2/uwsgi
 )
 pkg_build_deps=(
+  core/patch
+  python2/cffi
+  python2/pip
 )
 pkg_env_sep=(
   ['PYTHONPATH']=':'
@@ -24,14 +34,19 @@ do_begin() {
   add_path_env 'PYTHONPATH' 'lib/python2.7/site-packages'
 }
 
+do_prepare() {
+  patch -p0 -i "$PLAN_CONTEXT/setup.patch"
+}
+
 do_build() {
   python setup.py build
 }
 
 do_install() {
-  python setup.py install \
-    --prefix="$pkg_prefix" \
-    --old-and-unmanageable # bypass egg install
+#  python setup.py install \
+#    --prefix="$pkg_prefix" \
+#    --old-and-unmanageable # bypass egg install
+  pip install --prefix="$pkg_prefix" .
 }
 
 do_strip() {
