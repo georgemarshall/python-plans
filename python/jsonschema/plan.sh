057d7b397a86953db8452ced647adf88e4ee6cf9
George Marshall <george@georgemarshall.name>
2017-05-08 23:31:41 -0700

    Update jsonschema
    Signed-off-by: George Marshall <george@georgemarshall.name>

--- python/jsonschema/plan.sh
+++ python/jsonschema/plan.sh
@@ -19,28 +19,14 @@ pkg_env_sep=(
   ['PYTHONPATH']=':'
 )
 
-do_begin() {
-  add_path_env 'PYTHONPATH' 'lib/python3.6/site-packages'
-}
-
 do_build() {
   python setup.py build
 }
 
 do_install() {
+  add_path_env 'PYTHONPATH' "$PYTHON_SITE_PACKAGES"
   python setup.py install \
     --prefix="$pkg_prefix" \
+    --no-compile \
     --old-and-unmanageable # bypass egg install
-}
-
-do_strip() {
-  do_default_strip
-
-  # Remove tests and bytecode files
-  find "$pkg_prefix" -depth \
-    \( \
-      \( -type d -a -name test -o -name tests \) \
-      -o \
-      \( -type f -a -name '*.pyc' -o -name '*.pyo' \) \
-    \) -exec rm -rf '{}' +
 }
