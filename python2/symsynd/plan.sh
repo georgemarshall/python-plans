e90fa2c86ac6d2ac098236847514d67c96295d99
George Marshall <george@georgemarshall.name>
2017-05-09 00:03:25 -0700

    Update symsynd
    Signed-off-by: George Marshall <george@georgemarshall.name>

--- python2/symsynd/plan.sh
+++ python2/symsynd/plan.sh
@@ -1,14 +1,14 @@
 pkg_name=symsynd
 pkg_distname=${pkg_name}
-pkg_version=2.4.0
+pkg_version=3.0.0
 pkg_origin=python2
 pkg_license=('BSD-3-Clause')
 pkg_maintainer="George Marshall <george@georgemarshall.name>"
 pkg_description="Helps symbolicating crash dumps."
 pkg_upstream_url=http://github.com/getsentry/symsynd
 pkg_dirname=${pkg_distname}-${pkg_version}
-pkg_source=https://github.com/getsentry/symsynd/archive/${pkg_version}.tar.gz
-pkg_shasum=f77d60c837127541c9163c1b212e9b57e3338ecb034eb2291f5e8b9d3346fdf1
+pkg_source=https://pypi.org/packages/source/s/symsynd/${pkg_dirname}.zip
+pkg_shasum=796a7960dcf1e39cb192e9de4070b782d7777164758767f096c8c8fa20b445e4
 pkg_deps=(
   core/gcc-libs
   core/libffi
@@ -19,16 +19,13 @@ pkg_build_deps=(
   core/gcc
   core/make
   core/patch
+  core/rust
   python2/cffi
   python2/setuptools
 )
 pkg_env_sep=(
   ['PYTHONPATH']=':'
 )
-
-do_begin() {
-  add_path_env 'PYTHONPATH' 'lib/python2.7/site-packages'
-}
 
 do_prepare() {
   export CC="$(pkg_path_for gcc)/bin/gcc"
@@ -42,19 +39,9 @@ do_build() {
 }
 
 do_install() {
+  add_path_env 'PYTHONPATH' "$PYTHON_SITE_PACKAGES"
   python setup.py install \
     --prefix="$pkg_prefix" \
+    --no-compile \
     --old-and-unmanageable # bypass egg install
-}
-
-do_strip() {
-  do_default_strip
-
-  # Remove tests and bytecode files
-  find "$pkg_prefix" -depth \
-    \( \
-      \( -type d -a -name test -o -name tests \) \
-      -o \
-      \( -type f -a -name '*.pyc' -o -name '*.pyo' \) \
-    \) -exec rm -rf '{}' +
 }
